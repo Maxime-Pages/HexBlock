40968014fd457b9e61b47a35e581056e72d2dd7f
Nicolas MIDA <nicolas.mida@algosup.com>
2021-09-27 16:25:25 +0200

    upgrades

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Threading;
 
 
@@ -8,90 +8,87 @@ namespace HexBlock
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Bienvenue sur le jeu HEX");
-            // Console.ReadKey();
-            //Board game = new Board(3);
-            
-            //Console.WriteLine(game.Game(3,false) ? "P1 won" : "P2 won");
-            //game.DisplayTemp();
-            //Console.ReadKey();
-            // Int32.TryParse(s, i)
-        
-            
-
             System.Console.WriteLine("Welcome to the HexGame");
             Thread.Sleep(1000); // do a pause between text
-            System.Console.WriteLine("Who do you want to play against ? (AI or Player)");
-            String choiceOpponent = Console.ReadLine();
-            bool error = false;
-    
-            switch (choiceOpponent)
-                {
-                    case "A" :
-                    case "a" :  
-                    case "AI" : 
+            bool errorOpp = true;
+            while(errorOpp == true)
+            {
+                System.Console.WriteLine("Who do you want to play against ? (AI or Player)");
+                String choiceOpponent = Console.ReadLine();
+                switch (choiceOpponent.ToLower())
                     {
-                        System.Console.WriteLine("You will play with an AI");
-                        break;
-                    }
-                    case "P" :
-                    case "p" :
-                    case "Player" : 
-                    {
-                        System.Console.WriteLine("You will play against an another Player");
-                        break;
-                    }
+                        case "a" :  
+                        case "ai" : 
+                        {
+                            errorOpp = false;
+                            System.Console.WriteLine("You will play with an AI");
+                            break;
+                        }
+                        case "p" :
+                        case "player" : 
+                        {
+                            errorOpp = false;
+                            System.Console.WriteLine("You will play against an another Player");
+                            break;
+                        }
 
+                        default :
+                        {
+                            errorOpp = true;
+                            Console.Clear();
+                            System.Console.WriteLine("You didn't choose an opponent, please try again");
+                            break;
+                        }
+                    }
+            }
+            Thread.Sleep(1000);
+            bool errorSize = true;
+            while(errorSize == true)
+            {
+                System.Console.WriteLine("What about the size of the grid ? (11,13,19)");
+                bool success = false;
+                int size = 0;
+
+                while(!success)
+                {
+                    string s = Console.ReadLine();
+                    success = Int32.TryParse(s, out size);
+                }
+                switch(size)
+                {
+                    case 11 :
+                    {
+                        errorSize = false;
+                        System.Console.WriteLine("You choose a grid of 11x11");
+                        Board game = new Board(size);
+                        game.Game(size,false);
+                        break;
+                    }
+                    case 13 :
+                    {
+                        errorSize = false;
+                        System.Console.WriteLine("You choose a grid of 13x13");
+                        Board game = new Board(size);
+                        game.Game(size,false);
+                        break;
+                    }
+                    case 19 :
+                    {
+                        errorSize = false;
+                        System.Console.WriteLine("You choose a grid of 19x19");
+                        Board game = new Board(size);
+                        game.Game(size,false);
+                        break;
+                    }
                     default :
                     {
-                        System.Console.WriteLine("You didn't choose an opponent, please try again");
+                        errorSize = true;
+                        Console.Clear();
+                        System.Console.WriteLine("The requested size of the board is wrong, please try again");
                         break;
                     }
                 } 
-            
-            // System.Console.WriteLine(choiceOpponent);
-            // Console.ReadKey();
-            Thread.Sleep(1000);
-            System.Console.WriteLine("What about the size of the grid ? (11,13,19)");
-            bool success = false;
-            int size = 0;
-
-            while(!success)
-            {
-                string s = Console.ReadLine();
-                success = Int32.TryParse(s, out size);
-            }
-
-            switch(size)
-            {
-                case 11 :
-                {
-                    System.Console.WriteLine("You choose a grid of 11x11");
-                    Board game = new Board(size);
-                    game.Game(size,false);
-                    break;
-                }
-                case 13 :
-                {
-                    System.Console.WriteLine("You choose a grid of 13x13");
-                    Board game = new Board(size);
-                    game.Game(size,false);
-                    break;
-                }
-                case 19 :
-                {
-                    System.Console.WriteLine("You choose a grid of 19x19");
-                    Board game = new Board(size);
-                    game.Game(size,false);
-                    break;
-                }
-                default :
-                {
-                    System.Console.WriteLine("The requested size of the board is wrong, please try again");
-                    Console.ReadKey();
-                    break;
-                }
-            }        
+            }       
         }         
 
     }
