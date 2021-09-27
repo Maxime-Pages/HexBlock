cb7730dbb043ad37aa26155f9cb7b74e3248b014
Nicolas MIDA <nicolas.mida@algosup.com>
2021-09-27 14:04:20 +0200

    interface upgraded

--- src/Program.cs
+++ src/Program.cs
@@ -7,38 +7,81 @@ namespace HexBlock
     {
         static void Main(string[] args)
         {
-            // Board game = new Board(3);
-            // game.Game(3,false);
+            
 
             System.Console.WriteLine("Welcome to the HexGame");
-            Thread.Sleep(1500); // do a pause between text
+            Thread.Sleep(1000); // do a pause between text
             System.Console.WriteLine("Who do you want to play against ? (AI or Player)");
             String choiceOpponent = Console.ReadLine();
+            bool error = false;
+    
             switch (choiceOpponent)
+                {
+                    case "A" :
+                    case "a" :  
+                    case "AI" : 
+                    {
+                        System.Console.WriteLine("You will play with an AI");
+                        break;
+                    }
+                    case "P" :
+                    case "p" :
+                    case "Player" : 
+                    {
+                        System.Console.WriteLine("You will play against an another Player");
+                        break;
+                    }
+
+                    default :
+                    {
+                        System.Console.WriteLine("You didn't choose an opponent, please try again");
+                        break;
+                    }
+                } 
+            
+            // System.Console.WriteLine(choiceOpponent);
+            // Console.ReadKey();
+            Thread.Sleep(1000);
+            System.Console.WriteLine("What about the size of the grid ? (11,13,19)");
+            bool success = false;
+            int size = 0;
+
+            while(!success)
             {
-                case "A" :
-                case "a" :  
-                case "AI" : 
+                string s = Console.ReadLine();
+                success = Int32.TryParse(s, out size);
+}
+
+            switch(size)
+            {
+                case 11 :
                 {
-                     System.Console.WriteLine("You will play with an AI");
-                     break;
-                }
-                case "P" :
-                case "p" :
-                case "Player" : 
-                {
-                    System.Console.WriteLine("You will play against an another Player");
+                    System.Console.WriteLine("You choose a grid of 11x11");
+                    Board game = new Board(size);
+                    game.Game(size,false);
                     break;
                 }
-
+                case 13 :
+                {
+                    System.Console.WriteLine("You choose a grid of 13x13");
+                    Board game = new Board(size);
+                    game.Game(size,false);
+                    break;
+                }
+                case 19 :
+                {
+                    System.Console.WriteLine("You choose a grid of 19x19");
+                    Board game = new Board(size);
+                    game.Game(size,false);
+                    break;
+                }
                 default :
                 {
-                    System.Console.WriteLine("You didn't choose an opponent, please try again");
+                    System.Console.WriteLine("The requested size of the board is wrong, please try again");
+                    Console.ReadKey();
                     break;
                 }
-            }
-            System.Console.WriteLine(choiceOpponent);
-            Console.ReadKey();
-        }
+            }        
+        }         
     }
 }
