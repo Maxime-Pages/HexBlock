57faaf41f7a669f9f2eb807e4ca5ecf523d23009
Nicolas MIDA <nicolas.mida@algosup.com>
2021-09-27 11:43:57 +0200

    add choice opponent

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace HexBlock
 {
@@ -6,8 +7,38 @@ namespace HexBlock
     {
         static void Main(string[] args)
         {
-        Board game = new Board(3);
-        game.Game(3,false);
+            // Board game = new Board(3);
+            // game.Game(3,false);
+
+            System.Console.WriteLine("Welcome to the HexGame");
+            Thread.Sleep(1500); // do a pause between text
+            System.Console.WriteLine("Who do you want to play against ? (AI or Player)");
+            String choiceOpponent = Console.ReadLine();
+            switch (choiceOpponent)
+            {
+                case "A" :
+                case "a" :  
+                case "AI" : 
+                {
+                     System.Console.WriteLine("You will play with an AI");
+                     break;
+                }
+                case "P" :
+                case "p" :
+                case "Player" : 
+                {
+                    System.Console.WriteLine("You will play against an another Player");
+                    break;
+                }
+
+                default :
+                {
+                    System.Console.WriteLine("You didn't choose an opponent, please try again");
+                    break;
+                }
+            }
+            System.Console.WriteLine(choiceOpponent);
+            Console.ReadKey();
         }
     }
 }
