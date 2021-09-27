7f549ae014b47920152d3dac88cdc8b9b5f58ce1
Nicolas MIDA <nicolas.mida@algosup.com>
2021-09-27 11:02:25 +0200

    test

--- src/Program.cs
+++ src/Program.cs
@@ -6,21 +6,8 @@ namespace HexBlock
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
-            Console.WriteLine("Bienvenue sur le jeu du Hex");
-            /*
-            Board game = new Board(3);
-            game.Game(3,false);
-            */
-            Console.WriteLine("Who do you want to play against ?");
-            String choice = System.Console.ReadLine();
-            // Console.ReadKey();
-    
-=======
-           Board game = new Board(3);
+        Board game = new Board(3);
         game.Game(3,false);
->>>>>>> 50623a8b8f763159207e1af1aec2b2c98c3fa703
         }
     }
 }
-
