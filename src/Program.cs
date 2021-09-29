110e14389b75e519159ed8e617df03018c1cbb2f
Nicolas MIDA <nicolas.mida@algosup.com>
2021-09-29 14:41:20 +0200

    choice between graphical and console game

--- src/Program.cs
+++ src/Program.cs
@@ -10,11 +10,49 @@ namespace HexBlock
             System.Console.BackgroundColor=ConsoleColor.White;
             System.Console.ForegroundColor=ConsoleColor.Black;
             Console.Clear();
-            System.Console.WriteLine("Welcome to the HexGame");
+            bool errorChoiceApp = true; // true is a correct choice and false is an error
+
+            System.Console.WriteLine("Welcome to the menu, before starting we would like to know");
+            while(errorChoiceApp)
+            {
+                System.Console.WriteLine("Which application do you prefer ? (G for Graphical and C for Console)");
+                String choiceApplication = Console.ReadLine();
+                switch (choiceApplication.ToLower()) 
+                {
+                    case "g" :
+                    case "graphical" :
+                    {
+                        errorChoiceApp = false;
+                        System.Diagnostics.Process.Start( @"C:\Program Files\Google\Chrome\Application\chrome.exe"); // actually this open chrome but in the future this will open the application
+                        System.Console.WriteLine("Game is starting... ");
+                        Thread.Sleep(2000);
+                        System.Environment.Exit(1); // console shutdown
+                        break;
+                    }
+                    case "c" :
+                    case "console" :
+                    {
+                        errorChoiceApp = false;
+                        System.Console.WriteLine("You choosed the console game");
+                        Thread.Sleep(2000);
+                        break;
+                    }
+                    default :
+                    {
+                        errorChoiceApp = true;
+                        Console.Clear();
+                        System.Console.WriteLine("You didn't choose a valid choice, please try again");
+                        break;
+                    }
+                }
+            }
+
+            Console.Clear();
+            System.Console.WriteLine("Welcome to the HexGame : console edition");
             Thread.Sleep(1000); // do a pause between text
             bool errorOpp = true; // I fixed the boolean on true to be able to enter on the loop
             bool opponnent = false ; // True for an AI, False for a player
-            while(errorOpp == true) // verify if the opponent ent<ered is valid
+            while(errorOpp == true) // verify if the opponent entered is valid
             {
                 System.Console.WriteLine("Who do you want to play against ? (AI or Player)");
                 String choiceOpponent = Console.ReadLine(); 
@@ -165,4 +203,5 @@ namespace HexBlock
                 }
         }         
     }
+    
 }
