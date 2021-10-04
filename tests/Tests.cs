df36f145ffdffeb0d429aed880c9f5b1c02dcaac
Aik <maxime.pages@algosup.com>
2021-10-04 09:29:34 +0200

    Added NUnit

--- tests/Tests.cs
+++ tests/Tests.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace HexBlock.Tests
 {
     class Tests
     {
-
+        [TEST]
     }
 }
