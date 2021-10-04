cc4f3581f5d0a9325e91abf554175ff2798b426b
Paul NOWAK <paul.nowak@algosup.com>
2021-10-04 10:40:39 +0200

    Décalage à droite

--- Program.cs
+++ Program.cs
@@ -16,6 +16,7 @@ namespace howto_hexagonal_grid
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
         }
     }
 }
