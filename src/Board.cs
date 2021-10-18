b55faf7c1d949345ffd7a45ce0c4d5a41045899c
Aik <maxime.pages@algosup.com>
2021-10-18 17:05:32 +0200

    Merged with console App

---
+++ src/Board.cs
@@ -0,0 +1,324 @@
+using System;
+namespace HexBlock
+{
+    public enum Difficulty
+    {
+        EASY,
+        MEDIUM,
+        HARD,
+        IMPOSSIBLE,
+        NULL
+    }
+    partial class Board
+    {
+        #region Attributes
+
+
+
+
+        private int size;
+        private Spot[,] grid;
+        private bool cturn;
+
+        private int turn;
+        private bool solo;
+        private Difficulty difficulty = Difficulty.NULL;
+        #endregion
+        #region Getters
+        public int GetSize()
+        {
+            return size;
+        }
+
+        public bool GetSolo()
+        {
+            return solo;
+        }
+
+        public Difficulty GetDifficulty()
+        {
+            return difficulty;
+        }
+
+        public bool GetCTurn()
+        {
+            return cturn; // true p1 || false p2
+        }
+
+        public int GetTurn()
+        {
+            return turn; // return the global turn of the game 
+        }
+
+        public Spot[,] GetGrid()
+        {
+            return grid;
+        }
+        #endregion
+
+        /*
+        public (int,int) ChooseSpot()
+        {
+            bool success = false;
+            int x = 0;
+            int y = 0;
+            while (!success)
+            {
+                Console.Clear();
+                Console.WriteLine("Player " + (this.cturn ? "1" : "2"));
+                System.Console.WriteLine("Enter x Coordinates:");
+                string s = Console.ReadLine();
+                success = Int32.TryParse(s, out x);
+                while (!success)
+                {
+                    System.Console.WriteLine("Pls can you ReEnter x Coordinates not a letter:");
+                    string a = Console.ReadLine();
+                    success = Int32.TryParse(a, out x);
+                }
+                while (x >= size)
+                {
+                    Console.Clear();
+                    System.Console.WriteLine("Pls can you ReEnter x Coordinates the maximum is " + (size - 1) + " :");
+                    string a = Console.ReadLine();
+                    success = Int32.TryParse(a, out x);
+                }
+
+            }
+            success = false;
+            while (!success)
+            {
+                Console.Clear();
+                Console.WriteLine("Player " + (this.cturn ? "1" : "2"));
+                System.Console.WriteLine("Enter y Coordinates:");
+                string s = Console.ReadLine();
+                success = Int32.TryParse(s, out y);
+                while (!success)
+                {
+                    System.Console.WriteLine("Pls can you ReEnter y Coordinates not a letter:");
+                    string a = Console.ReadLine();
+                    success = Int32.TryParse(a, out y);
+                }
+                while (y >= size)
+                {
+                    System.Console.WriteLine("Pls can you ReEnter y Coordinates the maximum is " + (size - 1) + " :");
+                    string a = Console.ReadLine();
+                    success = Int32.TryParse(a, out y);
+                }
+            }
+            return (x, y);
+        }
+        */
+        private bool Haswon(bool player)
+        {
+            return Pathfinding.pathfind(player, this.grid);
+        }
+
+        private bool Legal((int, int) cor)
+        {
+            return cor.Item1 < this.size &&
+                cor.Item1 >= 0 &&
+                cor.Item2 < this.size &&
+                cor.Item2 >= 0 &&
+                this.grid[cor.Item1, cor.Item2].IsEmpty();
+        }
+
+        public bool ColorSpot((int, int) cor, bool player)
+        {
+            if (Legal(cor))
+            {
+                grid[cor.Item1, cor.Item2].Color(player);
+                return true;
+            }
+
+            return false;
+        }
+        public Board(int size, bool solo = false, Difficulty diff = Difficulty.NULL)
+        {
+            if (!IsSizeValid(size))
+                throw new Exception("Invalid Size");
+            this.size = size;
+            this.grid = new Spot[size, size];
+            this.cturn = true;
+            this.turn = 1;
+            this.solo = solo;
+            this.difficulty = diff;
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    grid[i, j] = new Spot(i, j);
+                }
+            }
+        }
+        /// <summary>
+        /// Blabla
+        /// </summary>
+        /// <param name="size"></param>
+        /// <returns></returns>
+        public static bool IsSizeValid(int size)
+        {
+            switch (size)
+            {
+                case 3:
+                case 11:
+                case 13:
+                case 19:
+                    return true;
+                default:
+
+                    return false;
+
+            }
+        }
+        public static bool StartGame(int size, bool solo, Difficulty difficulty = Difficulty.NULL)
+        {
+            if (solo) // if true == solo | false == player
+            {
+                Board board = new Board(size, solo, difficulty);
+                board.Board_display();
+                return board.GameSolo();
+            }
+            else
+            {
+                Board board = new Board(size);
+                board.Board_display();
+                return board.GameMulti();
+            }
+        }
+
+        public static bool PlayCursor()
+        {
+            Board board = new Board(11, false);
+            return board.CursorMulti();
+        }
+
+        public bool GameSolo()
+        {
+            while (true)
+            {
+                if (cturn)
+                {
+                    (int, int) chosen;
+                    do
+                    {
+                        chosen = InputCoo();
+                    } while (!Play(chosen));
+                }
+                else
+                {
+                    AIPlay();
+                }
+                if (Haswon(cturn))
+                {
+                    return cturn;
+                }
+
+                this.Board_display();
+                Console.ReadKey();
+            }
+        }
+
+        private (int, int) InputCoo()
+        {
+            try
+            {
+                string[] strings = Console.ReadLine().Split(',');
+                if (Int32.TryParse(strings[0], out int x) && Int32.TryParse(strings[1], out int y))
+                {
+                    return (x, y);
+                }
+                return (-1, -1);
+            }
+            catch (Exception e)
+            {
+                return (-1, -1);
+            }
+        }
+
+
+        public bool GameMulti()
+        {
+
+            while (true)
+            {
+                (int, int) chosen;
+                do
+                {
+                    chosen = InputCoo();
+                } while (Play(chosen));
+                if (Haswon(cturn))
+                {
+                    return cturn;
+                }
+
+                this.Board_display();
+                Console.ReadKey();
+
+            }
+        }
+        public bool Play((int, int) coo)
+        {
+            if (!ColorSpot(coo, cturn))
+                return false;
+            cturn = !cturn;
+            if (cturn)
+                turn++;
+            return true;
+        }
+
+        public void AIPlay()
+        {
+            if (difficulty == Difficulty.EASY)
+            {
+                (int, int) chosen;
+                do
+                {
+                    chosen = AI.RandomAI(size);
+                } while (!Play(chosen));
+            }
+            else
+            {
+                Play(AI.GenerateAIMove(grid, difficulty));
+            }
+        }
+
+
+        public bool CursorMulti()
+        {
+            while (true)
+            {
+                int x = 0;
+                int y = 0;
+                bool enter = false;
+                do
+                {
+                    Board_display(x, y);
+                    (x, y, enter) = Cursor(x, y);
+                } while (!enter || Play((y, x)));
+                if (Haswon(cturn))
+                {
+                    return cturn;
+                }
+            }
+        }
+        public bool CursorSolo()
+        {
+            while (true)
+            {
+                (int, int) chosen;
+                do
+                {
+                    chosen = InputCoo();
+                } while (Play(chosen));
+                if (Haswon(cturn))
+                {
+                    return cturn;
+                }
+
+                this.Board_display();
+                Console.ReadKey();
+
+            }
+        }
+    }
+}
---
+++ src/display.cs
@@ -0,0 +1,317 @@
+using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace HexBlock
+{
+    partial class Board
+    {
+        public void Board_display(int x = -1, int y = -1)
+        {
+            Console.Clear();
+            draw_banner_and_turn();
+            display_row_column_numbers();
+            display_second_row_column_numbers();
+            display_row_one_by_one_and_blue_number(x, y);
+            display_row_red_number();
+        }
+
+
+        public void draw_banner_and_turn()
+        {
+
+            string spaces = "";
+            string barre = "-";
+            int boardSize = this.size;
+            int indent = 0;
+            int bannerSize = (int)((2 * indent) + 7 + boardSize * 3 + Math.Log10(boardSize)); // The size of banner depend of the size of the Board
+            int numberTurn = (int)Math.Log10(turn) + 1;
+            int bannerPadding = (bannerSize - (11 + numberTurn)) / 2;
+
+            Console.Clear(); // clear the console for the visual
+
+            // we print the banner for the turn
+
+            for (int i = 0; i < indent - 1; i++)
+            {
+                spaces += " ";
+            }
+
+            System.Console.WriteLine('\n');
+            for (int i = 0; i < bannerPadding; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.Write(barre);
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green; // make the turn in the color green
+            Console.Write("Turn " + turn + " ");
+            Console.ForegroundColor = ConsoleColor.Black; // stay the -- in the color black
+
+
+            for (int i = 0; i < bannerPadding; i++)
+            {
+                Console.Write(barre);
+            }
+
+            if (numberTurn % 2 == 0)
+                Console.Write(barre);
+
+            Console.Write('\n' + spaces + '\n');
+        }
+
+
+
+        // Firstly we print the first row of column numbers
+
+        public void display_row_column_numbers()
+        {
+            int boardSize = this.size;
+            string spaces = "";
+            int padding = (int)Math.Log10(boardSize);
+
+
+            Console.Write(spaces);
+            for (int i = 0; i < padding + 2; i++) // we have +2 for print the number and have a good padding
+            {
+                Console.Write(" ");
+            }
+
+            for (int j = 0; j < boardSize; j++)
+            {
+                if (j < 10)
+                {
+                    Console.Write("  ");
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write(j / 10 + " ");
+                    Console.ForegroundColor = ConsoleColor.Black;
+                }
+            }
+
+            Console.Write('\n');
+        }
+
+        //Secondly we print the second row of column numbers
+        public void display_second_row_column_numbers()
+        {
+            int boardSize = this.size;
+            string spaces = "";
+            int padding = (int)Math.Log10(boardSize);
+
+
+            Console.Write(spaces);
+            for (int i = 0; i < padding + 3; i++)
+            {
+
+                Console.Write(" ");
+
+            }
+
+            for (float j = 0; j < boardSize; j++)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write(j % 10 + " ");
+                Console.ForegroundColor = ConsoleColor.Black;
+
+            }
+
+            Console.Write('\n');
+        }
+
+        //Eventually we print the rows one by one
+        public void display_row_one_by_one_and_blue_number(int x, int y)
+        {
+            int boardSize = this.size;
+            string spaces = "";
+            int padding = (int)Math.Log10(boardSize);
+            int intlength;
+
+            for (int i = boardSize - 1; i >= 0; i--)
+            {
+                Console.Write(spaces);
+
+                // we put the board on the left to make it a parallelogram
+
+                for (int j = 0;
+                    j < boardSize - i - 1;
+                    j++) // we have -1 to make a difference between the row underneath and the row previously
+                {
+                    Console.Write(" ");
+                }
+
+                intlength = (i == 0) ? 0 : (int)Math.Log10(i);
+                for (int j = 0; j < padding - intlength; j++)
+                {
+                    Console.Write(" ");
+                }
+
+
+                // Print the row number 
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.Write(" " + i + "  ");
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                // Print the color, it depend of the coordinate say before
+
+                for (int j = 0; j < boardSize; j++)
+                {
+                    if ((i, j) == (x, y))
+                    {
+                        Console.Write("■ ");
+                        continue;
+                    }
+                    Console.ForegroundColor = this.grid[j, i].IsEmpty() ? ConsoleColor.Black :
+                        this.grid[j, i].GetColor() ? ConsoleColor.Red : ConsoleColor.Blue;
+                    Console.Write((this.grid[j, i].IsEmpty() ? "_" : this.grid[j, i].GetColor() ? "■" : "■") + " ");
+                    Console.ForegroundColor = ConsoleColor.Black;
+                }
+
+                // We print the row number same before but for the right
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.Write(" " + i + '\n');
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                intlength = (i == 0) ? 0 : (int)Math.Log10(i);
+                for (int j = 0; j < padding - intlength; j++)
+                {
+                    Console.Write(" ");
+
+
+                }
+            }
+        }
+
+        public void display_row_red_number()
+        {
+            int boardSize = this.size;
+            string spaces = "";
+            int padding = (int)Math.Log10(boardSize);
+            int indent = 0;
+            int bannersize =
+                (int)((2 * indent) + 7 + boardSize * 3 +
+                       Math.Log10(boardSize)); // The size of banner depend of the size of the Board
+            string barre = "-";
+
+            //Firstly we print first row of column numbers
+
+            Console.Write(spaces);
+            for (int i = 0; i < boardSize + padding + 4; i++)
+            {
+
+                Console.Write(" ");
+
+            }
+
+            for (int j = 0; j < boardSize; j++)
+            {
+                if (j >= 10)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write(j / 10 + " ");
+                    Console.ForegroundColor = ConsoleColor.Black;
+                }
+
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write(j + " ");
+                    Console.ForegroundColor = ConsoleColor.Black;
+                }
+
+            }
+
+            Console.Write('\n');
+
+            // Print second row of column numbers
+
+            Console.Write(spaces);
+            for (int i = 0; i < boardSize + padding + 5; i++)
+            {
+
+                Console.Write(" ");
+
+            }
+
+            for (int j = 0; j < boardSize; j++)
+            {
+                if (j < 10)
+                {
+
+                    Console.Write("  ");
+
+                }
+
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write(j % 10 + " ");
+                    Console.ForegroundColor = ConsoleColor.Black;
+                }
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < bannersize; i++)
+                Console.Write(barre);
+
+        }
+
+
+
+        public (int, int, bool) Cursor(int x, int y)
+        {
+            
+            bool enter = false;
+            ConsoleKeyInfo input = Console.ReadKey();
+
+            switch (input.Key)
+            {
+                case ConsoleKey.UpArrow:
+                    if (x != size - 1)
+                        x++;
+                    else
+                    {
+                        x = 0;
+                    }
+                    break;
+                case ConsoleKey.DownArrow:
+                    if (x != 0)
+                        x--;
+                    else
+                    {
+                        x = size - 1;
+                    }
+                    break;
+                case ConsoleKey.LeftArrow:
+                    if (y != 0)
+                        y--;
+                    else
+                    {
+                        y = size - 1;
+                    }
+
+                    break;
+                case ConsoleKey.RightArrow:
+                    if (y != size - 1)
+                        y++;
+                    else
+                    {
+                        y = 0;
+                    }
+                    break;
+                case ConsoleKey.Enter:
+
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    enter = true;
+                    break;
+
+            }
+
+            return (x, y, enter);
+
+        }
+
+    }
+}
